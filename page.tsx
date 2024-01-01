3718665dcfb517139aef79e4a8a19610a95456ea
grdbiz <34131495+grdbiz@users.noreply.github.com>
2024-01-02 06:03:23 +1000

    Add files via upload
    Follow these steps to set up the project locally on your machine.
    
    Prerequisites
    
    Make sure you have the following installed on your machine:
    
        Git
        Node.js
        npm (Node Package Manager)
    
    Cloning the Repository
    
    git clone https://github.com/adrianhajdin/pricewise.git
    cd pricewise
    
    Installation
    
    Install the project dependencies using npm:
    
    npm install
    
    Set Up Environment Variables
    	
    https://www.youtube.com/watch?v=lh9XVGv6BHs
    Web Scraping Full Course 2024 | Build and Deploy eCommerce Price Tracker
    
    JavaScript Mastery
    
    THE PRICEWISE GITHUB
    https://github.com/adrianhajdin/pricewise

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page - To get this working - Needed to perform many other configuration, that were not contained within this video.</div>
+  )
+}
+
+export default page
